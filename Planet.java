8daa4c04ec0c6c4a26cecfd7afb5f46efd1456fa
bai1110 <baixuechun19951@gmail.com>
2022-01-14 17:39:00 -0500

    first

---
+++ Planet.java
@@ -0,0 +1,21 @@
+public class Planet {
+    double xxPos;
+    double yyPos;
+    double xxVel;
+    double yyVel;
+    double mass;
+    String imgFileName;
+
+    public Planet(double xP, double yP, double xV, double yV, double m, String img){
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+
+
+
+}
